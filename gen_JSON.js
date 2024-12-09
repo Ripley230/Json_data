062a71495e466d0029fa6a919a9434741dc75120
Quentin <128997350+Ripley230@users.noreply.github.com>
2024-12-09 09:36:07 +0100

    Update gen_JSON.js
    9/12

--- gen_JSON.js
+++ gen_JSON.js
@@ -1,5 +1,5 @@
 const url_github_json_raw = "";
 const token_perso_github = "";
 const nom_utilisateur_github = "";
-const nom_repo_github = "";
+const nom_repo_github = "https://raw.githubusercontent.com/Ripley230/Json_data/refs/heads/main/data_page_accueil.json";
 const chemin_fichier_dans_repo = "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";
